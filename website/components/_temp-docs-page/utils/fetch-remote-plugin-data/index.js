1972edfb177f89cd8bf9a845b95aa971e897fc43
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-02-23 23:53:25 -0500

    WIP set up merging of remote plugins into nav data

--- website/components/_temp-docs-page/utils/fetch-remote-plugin-data/index.js
+++ website/components/_temp-docs-page/utils/fetch-remote-plugin-data/index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const fetchGithubFile = require('../fetch-github-file')
 const PLUGINS_FILE = 'data/_docs-remote-plugins.json'
+const NAV_DATA_FILE = 'data/_docs-nav-data-migrated.json'
 
 //
 //
@@ -15,12 +16,17 @@ const COMPONENT_TYPES = [
   'provisioners',
 ]
 
+const navDataFile = path.join(process.cwd(), NAV_DATA_FILE)
+const navDataJson = fs.readFileSync(navDataFile, 'utf-8')
+const navDataPacker = JSON.parse(navDataJson, null, 2)
+//
 const pluginsFilePath = path.join(process.cwd(), PLUGINS_FILE)
 const pluginsDataJson = fs.readFileSync(pluginsFilePath, 'utf-8')
 const pluginsData = JSON.parse(pluginsDataJson, null, 2)
-gatherRemotePlugins(pluginsData)
+gatherRemotePlugins(pluginsData, navDataPacker)
 
-async function gatherRemotePlugins(pluginsData) {
+async function gatherRemotePlugins(pluginsData, navData) {
+  //
   const allPluginData = await Promise.all(
     pluginsData.map(async (pluginEntry) => {
       return await Promise.all(
@@ -35,6 +41,8 @@ async function gatherRemotePlugins(pluginsData) {
   // TODO - should error if slotting into tree would replace plugin
   // TODO - what should the title of the nested tree be?
   console.log(allPluginData)
+  console.log(navData)
+
   return true
 }
 

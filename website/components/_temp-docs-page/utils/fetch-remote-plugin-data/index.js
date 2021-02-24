921d32a0e135d1c931cc53374ee1f2eae6628354
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2021-02-23 23:27:03 -0500

    Rough in script to fetch remote plugin components

---
+++ website/components/_temp-docs-page/utils/fetch-remote-plugin-data/index.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const fs = require('fs')
+const path = require('path')
+const fetchGithubFile = require('../fetch-github-file')
+const PLUGINS_FILE = 'data/_docs-remote-plugins.json'
+
+//
+//
+//
+
+const COMPONENT_TYPES = [
+  'builders',
+  'datasources',
+  'post-processors',
+  'provisioners',
+]
+
+const pluginsFilePath = path.join(process.cwd(), PLUGINS_FILE)
+const pluginsDataJson = fs.readFileSync(pluginsFilePath, 'utf-8')
+const pluginsData = JSON.parse(pluginsDataJson, null, 2)
+gatherRemotePlugins(pluginsData)
+
+async function gatherRemotePlugins(pluginsData) {
+  const allPluginData = await Promise.all(
+    pluginsData.map(async (pluginEntry) => {
+      return await Promise.all(
+        COMPONENT_TYPES.map(async (type) => {
+          return { type, navData: await gatherPluginBranch(pluginEntry, type) }
+        })
+      )
+    })
+  )
+  console.log(allPluginData)
+  return true
+}
+
+async function gatherPluginBranch(pluginEntry, component) {
+  const { repo, branch, artifactDir } = pluginEntry
+  const navDataFilePath = `${artifactDir}/${component}/nav-data.json`
+  const fileResult = await fetchGithubFile({
+    repo,
+    branch,
+    filePath: navDataFilePath,
+  })
+  const navData = JSON.parse(fileResult)
+  return navData
+}
